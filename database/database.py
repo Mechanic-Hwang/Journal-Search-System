4d852572e84856ed066e6c7ccd5d1b7b015a7957
MeiD <yogiyjhuang@gmail.com>
2025-05-01 00:38:23 +0800

    新增get_db方法

--- database/database.py
+++ database/database.py
@@ -6,3 +6,14 @@ DATABASE_URL = "sqlite:///./journal.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+# database.py
+from sqlalchemy.orm import Session
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
